936cbd3d21580854849b78f480f6b7a18f86b0dc
austonyb <auston.youngblood@gmail.com>
2022-10-14 10:19:38 -0600

    Completed javascript elements in about.js for part 2.

--- about.js
+++ about.js
@@ -4,11 +4,18 @@ console.log("hello world");
 
 function handleSubmit(evt) {
 	evt.preventDefault();
-	
+	alert('Form Submitted successfully.')
 	console.log('form submit');
 }
 
+function compliment () {
+	alert("You found the easter egg! You're one smart cookie!")
+}
 
 let form = document.querySelector('#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+const image = document.querySelector('img')
+
+image.addEventListener('mouseover', compliment)
