8f9cb795532968ebb0751a5cf0b2a0d1dd8d67e5
austonyb <auston.youngblood@gmail.com>
2022-10-14 10:57:40 -0600

    Added profile.js and modified styles in styles.css to make it more consistent with my other pages.

---
+++ profile.js
@@ -0,0 +1,19 @@
+const fvtColor = document.getElementById('color')
+const fvtPlace = document.getElementById('place')
+const fvtRitual = document.getElementById('ritual')
+
+const myFvtColor = () => {
+    alert('My favorite color is blue.')
+}
+
+const myFvtPlace = () => {
+    alert('My favorite place is Hawaii.')
+}
+
+const myFvtRitual = () => {
+    alert('My favorite ritual is getting together with family and friends on holidays.')
+}
+
+fvtColor.addEventListener('click', myFvtColor)
+fvtPlace.addEventListener('click', myFvtPlace)
+fvtRitual.addEventListener('click', myFvtRitual)
